161b8ce6dc60c9c567f1b759f06563b0b672812b
LoaDeadd <vvolkv81494@gmail.com>
2023-11-30 20:38:46 +0300

    start test

--- src/main/BL/build.gradle.kts
+++ src/main/BL/build.gradle.kts
@@ -14,32 +14,23 @@ repositories {
 }
 
 dependencies {
-//    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test"))
 
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("org.slf4j:slf4j-nop:$slf4j_version")
     implementation("org.slf4j:slf4j-api:$slf4j_version")
 
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-    testImplementation("org.assertj:assertj-core:3.24.2")
-
-    implementation("io.qameta.allure:allure-bom:2.24.0")
+    testImplementation("io.qameta.allure:allure-bom:2.24.0")
     testImplementation("io.qameta.allure:allure-junit5:2.24.0")
 }
 
 tasks.test {
     useJUnitPlatform()
-    testLogging {
-        events ("PASSED", "FAILED", "SKIPPED", "STANDARD_OUT", "STANDARD_ERROR")
-    }
-    testLogging.showStandardStreams = true
-
-    if (project.hasProperty("excludeTests")) {
-        exclude(project.property("excludeTests").toString())
-    }
+//    testLogging {
+//        events ("PASSED", "FAILED", "SKIPPED", "STANDARD_OUT", "STANDARD_ERROR")
+//    }
+//    testLogging.showStandardStreams = true
 }
 
 kotlin {
